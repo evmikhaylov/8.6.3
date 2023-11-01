379272c2070c574086d9e6a9124970f5e8c9073f
Evgenii Mikhaylov <zealknuckle@gmail.com>
2023-11-01 17:44:01 +0300

    Добавьте файлы проекта.

---
+++ 8.6.3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8._6._3
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			HashSet<int> numbers = new HashSet<int>();
+
+			Console.WriteLine("Вводите числа");			
+			while (true)
+			{
+				int number = Int32.Parse(Console.ReadLine());
+				if (numbers.Contains(number))
+					Console.WriteLine("Число уже вводилось ранее");
+				else
+				{
+					numbers.Add(number);
+                    Console.WriteLine("Число сохранено");
+                }
+                
+			}
+		}
+	}
+}
